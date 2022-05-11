7399891161dbe6f73f3dab19d6759968cabfcacc
Ellie yup <yupyujung@gmail.com>
2022-05-11 16:21:02 +0200

    rewrite2

--- js/main.js
+++ js/main.js
@@ -1,45 +1,67 @@
-const inputList = document.querySelectorAll("#open_info1, #open_info2, #open_info3"); // select all open buttons
-const containerList = document.querySelectorAll(".container1, .container2, .container3"); // select all container classes
-const outputList = document.querySelectorAll("#close_info1, #close_info2, #close_info3"); // select all close buttons
+// Without selecting method, id can be used because id is an unique value
+//open event - when users click each categories
+open_info1.addEventListener ("click", () => {
+    container1.classList.add("show");
+    });
+    open_info2.addEventListener ("click", () => {
+    container2.classList.add("show");
+    });
+    open_info3.addEventListener ("click", () => {
+    container3.classList.add("show");
+    });
 
-//set object depends on the data
+
+
+// close event - when users click the back button, it will turn on the main page
+    close_info1.addEventListener ("click", ()=> {
+    container1.classList.remove("show");
+    });
+    close_info2.addEventListener ("click", ()=> {
+    container2.classList.remove("show");
+    });
+    close_info3.addEventListener ("click", ()=> {
+    container3.classList.remove("show");
+    });
+    
+//set objects depends on the data 
+// Each object describes the variable information
 const data = [
-  {
-  params: "%",
-  initValue: 100,
-  checkTime: 2000,
-    endValue: 0,
-  type : "decrease"
-},
-  {
-  params: "KM",
-  initValue: 5040000,
-  checkTime: 20,
-  endValue : 0,
-  type : "decrease"
-},
-  {
-  params: "m/s2",
-  initValue: 9.81,
-  checkTime: 4000,
-  endValue : 0,
-  type : "decrease"
-},
-  {
-  params: "%",
-  initValue: 100,
-  checkTime: 2000,
-  endValue : 0,
-  type : "decrease"
-},
-  {
+  { //fuel
   params: "%",
   initValue: 100,
   checkTime: 2000,
   endValue: 0,
   type : "decrease"
 },
-  {
+  { //distance traveled
+  params: "KM",
+  initValue: 5040000,
+  checkTime: 20,
+  endValue : 0,
+  type : "decrease"
+},
+  { //gravity
+  params: "m/s2",
+  initValue: 9.81,
+  checkTime: 4000,
+  endValue : 0,
+  type : "decrease"
+},
+  { // food
+  params: "%",
+  initValue: 100,
+  checkTime: 2000,
+  endValue : 0,
+  type : "decrease"
+},
+  { //water
+  params: "%",
+  initValue: 100,
+  checkTime: 2000,
+  endValue: 0,
+  type : "decrease"
+},
+  { //speed
   params: "kph",
   initValue: 800,
   checkTime: 400,
@@ -50,36 +72,12 @@ const data = [
 ]
 
 
-Array.from(inputList).forEach(function(el) { //select each ids in the inputList 
-    //if user click the open button, the containers will show up 
-    open_info1.addEventListener ("click", () => {
-    container1.classList.add("show");
-    });
-    open_info2.addEventListener ("click", () => {
-    container2.classList.add("show");
-    });
-    open_info3.addEventListener ("click", () => {
-    container3.classList.add("show");
-    });
-  });
-
-  Array.from(outputList).forEach(function(el) { //select each ids in the outputList 
-    //if user click the open button, the containers will remove on the screen
-      close_info1.addEventListener ("click", ()=> {
-      container1.classList.remove("show");
-      });
-      close_info2.addEventListener ("click", ()=> {
-      container2.classList.remove("show");
-      });
-      close_info3.addEventListener ("click", ()=> {
-      container3.classList.remove("show");
-      });
-  });
+  
 
 
-// set variable 
+// set variable function - to keep changing information values 
 const intervalFn = (element,{params,initValue,checkTime,endValue,type}) => {
-  let num = initValue;
+  let num = initValue; // when time goes, numbers will be changed
   const timer = setInterval(() => {
     if (type === "increase") {
       ++num;
@@ -99,8 +97,8 @@ const intervalFn = (element,{params,initValue,checkTime,endValue,type}) => {
   const decrease = document.querySelectorAll(".decrease"); 
   const increase = document.getElementById("speed");
   
-  const 요소집합군 = [...decrease , increase]
-  // select elements which decrease the number
-  요소집합군.forEach((요소선수,선수번호) => {
-  intervalFn(요소선수,data[선수번호]);
+  const elList = [...decrease , increase] // select array and id 
+//each elements will be affected by the function that I set 
+  elList.forEach((el,index) => {
+  intervalFn(el,data[index]);
 })
